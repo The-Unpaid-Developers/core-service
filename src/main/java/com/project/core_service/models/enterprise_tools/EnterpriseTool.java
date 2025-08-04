c835935ce6bfbcb1067f32fe446552db941626b3
suo-haoran <suohaoran99@gmail.com>
2025-08-04 12:17:07 +0800

    Add overarching data structure

---
+++ src/main/java/com/project/core_service/models/enterprise_tools/EnterpriseTool.java
@@ -0,0 +1,35 @@
+package com.project.core_service.models.enterprise_tools;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.mongodb.lang.NonNull;
+import com.project.core_service.models.shared.VersionedSchema;
+
+import lombok.Data;
+
+@Document
+@Data
+public class EnterpriseTool implements VersionedSchema {
+    @Id
+    private String id;
+
+    @NonNull
+    private Tool tool;
+
+    @NonNull
+    private OnboardingStatus onboarded;
+
+    // Configurations, pipelines, APIs
+    // Explanation if tool not onboarded
+    private String integrationStatus;
+
+    // Blockers, non-compliance, risk notes
+    @NonNull
+    private String issues;
+
+    @NonNull
+    private String solutionOverviewId;
+
+    private int version;
+}
