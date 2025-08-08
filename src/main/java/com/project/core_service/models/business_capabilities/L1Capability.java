3834bf0eb2cc34351a6fb790508aa518f0bc7c80
suo-haoran <suohaoran99@gmail.com>
2025-08-08 10:16:22 +0800

    Create test

--- src/main/java/com/project/core_service/models/business_capabilities/L1Capability.java
+++ src/main/java/com/project/core_service/models/business_capabilities/L1Capability.java
@@ -2,4 +2,6 @@ package com.project.core_service.models.business_capabilities;
 
 public enum L1Capability {
     // TODO: Add L1 Capabilities
+
+    UNKNOWN, // Placeholder Value
 }
--- src/main/java/com/project/core_service/models/business_capabilities/L3Capability.java
+++ src/main/java/com/project/core_service/models/business_capabilities/L3Capability.java
@@ -2,4 +2,6 @@ package com.project.core_service.models.business_capabilities;
 
 public enum L3Capability {
     // TODO: Add L3 Capabilities
+
+    UNKNOWN, // Placeholder Value
 }
