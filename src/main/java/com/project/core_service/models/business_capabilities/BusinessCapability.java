7ab091d5f7322b362d4580c25fff57510216941b
suo-haoran <suohaoran99@gmail.com>
2025-10-04 14:17:45 +0800

    chore: fix model for chatbot service

--- src/main/java/com/project/core_service/models/business_capabilities/BusinessCapability.java
+++ src/main/java/com/project/core_service/models/business_capabilities/BusinessCapability.java
@@ -4,10 +4,10 @@ import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.mongodb.lang.NonNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.List;
 
 @Document
 @AllArgsConstructor
@@ -17,12 +17,7 @@ public class BusinessCapability {
     @Id
     private String id;
 
-    @NonNull
-    private L1Capability l1Capability;
-    @NonNull
-    private L2Capability l2Capability;
-    @NonNull
-    private L3Capability l3Capability;
-
     private String remarks;
+
+    private List<Capability> capabilities;
 }
