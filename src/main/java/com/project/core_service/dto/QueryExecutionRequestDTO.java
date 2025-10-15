976f2cf2c580dd81d61baed364da168ff237f28e
Yue Zhen <smithquaz@gmail.com>
2025-10-15 10:39:47 +0800

    feat: query runner api

---
+++ src/main/java/com/project/core_service/dto/QueryExecutionRequestDTO.java
@@ -0,0 +1,39 @@
+package com.project.core_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO for query execution parameters.
+ *
+ * <p>
+ * This DTO allows users to provide additional filters and conditions
+ * when executing a stored query.
+ * </p>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class QueryExecutionRequestDTO {
+
+    /**
+     * The collection to query against.
+     * If not provided, it will be extracted from the stored query.
+     */
+    private String collection;
+
+    /**
+     * Maximum number of documents to return.
+     * Default is 100 if not specified.
+     */
+    private Integer limit;
+
+    /**
+     * Number of documents to skip.
+     * Used for pagination.
+     */
+    private Integer skip;
+}
