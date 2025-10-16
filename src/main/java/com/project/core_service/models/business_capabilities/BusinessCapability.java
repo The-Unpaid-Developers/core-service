9d28fd58b4b463775b0f57d4efe0ad025699c080
Mak Jo Ray, Jamond <142817084+JamondMak@users.noreply.github.com>
2025-10-16 13:16:16 +0800

    feat: diagram for business capabilities (#92)

--- src/main/java/com/project/core_service/models/business_capabilities/BusinessCapability.java
+++ src/main/java/com/project/core_service/models/business_capabilities/BusinessCapability.java
@@ -7,8 +7,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
-
 @Document
 @AllArgsConstructor
 @Data
