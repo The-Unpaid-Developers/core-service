1234a2453f4aab185cd151967d3f6eb1b7e1d45c
ying xuan <xuan2103yyx@gmail.com>
2025-10-21 14:55:14 +0800

    chore: fix sonarqube issue

--- src/main/java/com/project/core_service/services/LookupService.java
+++ src/main/java/com/project/core_service/services/LookupService.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -110,7 +111,7 @@ public class LookupService {
         }
     }
 
-    private CSVParser createCsvParser(BufferedReader reader) throws Exception {
+    private CSVParser createCsvParser(BufferedReader reader) throws IOException {
         return new CSVParser(reader, CSVFormat.DEFAULT
                 .builder()
                 .setHeader()
